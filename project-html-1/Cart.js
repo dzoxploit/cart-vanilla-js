498e39b0828bfa536892ab58c19e132cf07b2ee1
Didin Yahya <didinnuryahya@gmail.com>
2023-02-07 20:35:21 +0700

    first-commit

---
+++ project-html-1/Cart.js
@@ -0,0 +1,34 @@
+class Cart {
+  constructor() {
+    this.items_array = [];
+  }
+  tambahProduk(name, count) {
+    let alert;
+    for (var i in this.items_array) {
+      if (this.items_array[i][0] == name) {
+        this.items_array.splice(i, 1, [name, this.items_array[i][1] + count]);
+        alert = "ok";
+        break;
+      }
+    }
+
+    if (alert == null) {
+      this.items_array.push([name, count]);
+    }
+  }
+
+  hapusProduk(name) {
+    for (var i in this.items_array) {
+      if (this.items_array[i][0] == name) {
+        this.items_array.splice(0, i);
+        break;
+      }
+    }
+  }
+
+  tampilkanProduk() {
+    for (var i in this.items_array) {
+      console.log(this.items_array[i][0] + " (" + this.items_array[i][1] + ")");
+    }
+  }
+}
